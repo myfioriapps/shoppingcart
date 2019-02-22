5d736b4805eadbc55543d6cc7084266824c4c810
jamesfan2018 <fanfangxing@aliyun.com>
2019-02-22 07:58:31 +0000

    shoppingcart

---
+++ webapp/controller/BaseController.js
@@ -0,0 +1,120 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/core/UIComponent",
+	"sap/ui/core/routing/History",
+	"../model/cart"
+], function(Controller, MessageToast, UIComponent, History, cart) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.cart.controller.BaseController", {
+		cart: cart,
+		/**
+		 * Convenience method for accessing the router.
+		 * @public
+		 * @returns {sap.ui.core.routing.Router} the router for this component
+		 */
+		getRouter: function () {
+			return UIComponent.getRouterFor(this);
+		},
+
+		/**
+		 * Convenience method for getting the view model by name.
+		 * @public
+		 * @param {string} [sName] the model name
+		 * @returns {sap.ui.model.Model} the model instance
+		 */
+		getModel: function (sName) {
+			return this.getView().getModel(sName);
+		},
+
+		/**
+		 * Convenience method for setting the view model.
+		 * @public
+		 * @param {sap.ui.model.Model} oModel the model instance
+		 * @param {string} sName the model name
+		 * @returns {sap.ui.mvc.View} the view instance
+		 */
+		setModel: function (oModel, sName) {
+			return this.getView().setModel(oModel, sName);
+		},
+
+		/**
+		 * Getter for the resource bundle.
+		 * @public
+		 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+		 */
+		getResourceBundle: function () {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+		},
+
+		/**
+		 * Handler for the Avatar button press event
+		 * @public
+		 */
+		onAvatarPress: function () {
+			var sMessage = this.getResourceBundle().getText("avatarButtonMessageToastText");
+			MessageToast.show(sMessage);
+		},
+
+		/**
+		 * Sets the flexible column layout to one, two, or three columns for the different scenarios across the app
+		 * @param {string} sColumns the target amount of columns
+		 * @private
+		 */
+		_setLayout: function (sColumns) {
+			if (sColumns) {
+				this.getModel("appView").setProperty("/layout", sColumns + "Column" + (sColumns === "One" ? "" : "sMidExpanded"));
+			}
+		},
+
+		/**
+		 * Apparently, the middle page stays hidden on phone devices when it is navigated to a second time
+		 * @private
+		 */
+		_unhideMiddlePage: function () {
+			// TODO: bug in sap.f router, open ticket and remove this method afterwards
+			setTimeout(function () {
+				this.getOwnerComponent().getRootControl().byId("layout").getCurrentMidColumnPage().removeStyleClass("sapMNavItemHidden");
+			}.bind(this), 0);
+		},
+
+		/**
+		 * Navigate back
+		 */
+		onBack: function () {
+			this._setLayout("Two");
+			var oHistory = History.getInstance();
+			var oPrevHash = oHistory.getPreviousHash();
+			if (oPrevHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getRouter().navTo("home", {}, true);
+			}
+		},
+
+		/**
+		 * Called, when the add button of a product is pressed.
+		 * Saves the product, the i18n bundle, and the cart model and hands them to the <code>addToCart</code> function
+		 * @public
+		 */
+		onAddToCart : function () {
+			var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			var oEntry =  arguments[0].getSource().getBindingContext().getObject();
+			var oCartModel = this.getView().getModel("cartProducts");
+			cart.addToCart(oResourceBundle, oEntry, oCartModel);
+		},
+		/**
+		 * Clear comparison model
+		 * @protected
+		 */
+		_clearComparison: function (){
+			var oModel = this.getOwnerComponent().getModel("comparison");
+			oModel.setData({
+				category: "",
+				item1: "",
+				item2: ""
+			});
+		}
+	});
+});
